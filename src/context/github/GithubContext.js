93ed987c73e61de227fd6954b7d3f217ff514269
emirsahovic <emir.sahovic00@gmail.com>
2022-01-03 21:02:48 +0100

    useReducer hook instead of useState hook added

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,23 +1,36 @@
-import { createContext, useState } from 'react';
+import { createContext, useReducer } from 'react';
+import githubReducer from './GithubReducer';
 
 const GithubContext = createContext();
 
 const GITHUB_URL = "https://api.github.com";
 
 export const GithubProvider = ({ children }) => {
-    const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(true);
-
-    const fetchUsers = async () => {
-        const response = await fetch(`${GITHUB_URL}/users`);
-        const data = await response.json();
-        setUsers(data);
-        setLoading(false);
+    const initialState = {
+        users: [],
+        loading: false
     }
 
+    const [state, dispatch] = useReducer(githubReducer, initialState);
+
+    // Get initial users (testing)
+    const fetchUsers = async () => {
+        setLoading();
+        const response = await fetch(`${GITHUB_URL}/users`);
+        const data = await response.json();
+
+        dispatch({
+            type: 'GET_USERS',
+            payload: data,
+        })
+    }
+
+    // Set loading
+    const setLoading = () => dispatch({ type: 'SET_LOADING' })
+
     return <GithubContext.Provider value={{
-        users,
-        loading,
+        users: state.users,
+        loading: state.loading,
         fetchUsers
     }}>
         {children}
