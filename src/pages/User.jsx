a5e88f6f8152540fed012391cca51cacb4566092
emirsahovic <emir.sahovic00@gmail.com>
2022-01-05 21:41:20 +0100

    Repository list for a single user added

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -4,14 +4,17 @@ import GithubContext from '../context/github/GithubContext';
 import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import Spinner from '../components/layout/Spinner';
+import RepoList from '../components/repos/RepoList'
 
 function User() {
-    const { getUser, user, loading } = useContext(GithubContext);
+    const { getUser, user, loading, getUserRepos, repos } = useContext(GithubContext);
 
     const params = useParams();
 
     useEffect(() => {
         getUser(params.login);
+        getUserRepos(params.login);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const {
@@ -157,6 +160,8 @@ function User() {
                         </div>
                     </div>
                 </div>
+
+                <RepoList repos={repos} />
             </div>
         </>
     )
