3931056d1b524a2d27d6638b8858e654381ccb0c
emirsahovic <emir.sahovic00@gmail.com>
2022-01-07 11:54:05 +0100

    Search input centered

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -20,13 +20,13 @@ function UserSearch() {
     }
 
     return (
-        <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
+        <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-14 gap-8" style={{ marginLeft: "32rem" }}>
             <div>
                 <form onSubmit={handleSubmit}>
                     <div className="form-control">
                         <div className="relative">
-                            <input type="text" className="w-full pr-40 bg-gray-200 input input-lg text-black" placeholder="Search" value={text} onChange={handleChange} />
-                            <button type="submit" className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg">
+                            <input type="text" className="w-full pr-40 bg-gray-200 input input-lg text-black" placeholder="Search for users" value={text} onChange={handleChange} />
+                            <button type="submit" className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg btn-primary font-bold text-white">
                                 Go
                             </button>
                         </div>
